a94e05aec253357a45b3211f38c008fb64c3a683
levi934 <leviryago@gmail.com>
2025-05-20 15:49:03 -0300

    meu site

---
+++ MeuSiteComIA/js/script.js
@@ -0,0 +1,4 @@
+// Função para exibir uma mensagem quando o botão for clicado
+document.getElementById('mostrarMensagem').addEventListener('click', function () {
+    alert("Olá! Sou Lucas Silva De Oliveira, um entusiasta da tecnologia e estudante dedicado. Agradeço por visitar meu portfólio!");
+});
